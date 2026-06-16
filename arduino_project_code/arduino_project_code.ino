6074d74c3a9e3736ac9309d9ddb08a8c78ed219b
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-06-16 16:47:06 +0530

    Add ThingSpeak and make connection with dashboard

--- arduino_project_code/arduino_project_code.ino
+++ arduino_project_code/arduino_project_code.ino
@@ -1,3 +1,10 @@
+#include <Ethernet.h>        
+#include <ThingSpeak.h>
+
+unsigned long channelID = 3409407;            
+const char* writeAPIKey = "UTRP8URMKVNBOGBN"; 
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+EthernetClient client;
 
 const int moisturePinA = A0;
 const int moisturePinB = A1;
@@ -29,6 +36,9 @@ void setup() {
   digitalWrite(relayPinA, LOW);
   digitalWrite(relayPinB, LOW);
   digitalWrite(relayPinC, LOW);
+
+  Ethernet.begin(mac);
+  ThingSpeak.begin(client);
   
   Serial.println("Multi-Zone Irrigation System Started!");
 }
@@ -48,11 +58,12 @@ void loop() {
   Serial.print(" | Plant B - Moist: " + String(moistB) + " Light: " + String(lightB));
   Serial.println(" | Plant C - Moist: " + String(moistC) + " Light: " + String(lightC));
 
-
+  int pumpA = 0, pumpB = 0, pumpC = 0;
 
 
   if (moistA > dryThresholdA && lightA > minLightThreshold) {
     digitalWrite(relayPinA, HIGH);
+    pumpA = 1;
     Serial.println("-> Pump A: ON");
   } else {
     digitalWrite(relayPinA, LOW);
@@ -60,7 +71,8 @@ void loop() {
 
  
   if (moistB > dryThresholdB && lightB > minLightThreshold) {
-    digitalWrite(relayPinB, HIGH); 
+    digitalWrite(relayPinB, HIGH);
+    pumpB = 1; 
     Serial.println("-> Pump B: ON");
   } else {
     digitalWrite(relayPinB, LOW);  
@@ -69,9 +81,25 @@ void loop() {
 
   if (moistC > dryThresholdC && lightC > minLightThreshold) {
     digitalWrite(relayPinC, HIGH); 
+    pumpC = 1;
     Serial.println("-> Pump C: ON");
   } else {
     digitalWrite(relayPinC, LOW); 
+  }
+
+  ThingSpeak.setField(1, moistA);
+  ThingSpeak.setField(2, moistB);
+  ThingSpeak.setField(3, moistC);
+  ThingSpeak.setField(4, lightA);  
+  ThingSpeak.setField(5, pumpA);
+  ThingSpeak.setField(6, pumpB);
+  ThingSpeak.setField(7, pumpC);
+
+  int result = ThingSpeak.writeFields(channelID, writeAPIKey);
+  if (result == 200) {
+    Serial.println("ThingSpeak update successful!");
+  } else {
+    Serial.println("ThingSpeak update failed. Error: " + String(result));
   }
 
   Serial.println("------------------------------------");
